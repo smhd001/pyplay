d14121c4c2ff0dc17db557983d8793c383f494be
mohammad hashemy <smhd.hashemy@outlook.com>
2021-07-26 01:55:20 +0430

    this is a commit

---
+++ find.py
@@ -0,0 +1,78 @@
+import os
+import re
+from glob import glob
+from os.path import isdir
+from typing import Dict
+
+
+def is_played_before(s_name: str, series:list[Dict] ) -> bool or str:
+    for s in series:
+        if s_name in s["name"]:
+            return s
+    return False
+
+
+def find_dir(name: str) -> str:
+    for path in glob("**", recursive=True):
+        if name in path and isdir(path):
+            name = path
+            break
+    return name
+
+
+def find_ep(directory: str, episode: int) -> str:
+    a = []
+    print(directory)
+    for ep in glob(directory + "/**"):
+        if f"{episode:02d}" in ep:
+            a.append(ep)
+    if len(a) == 1:
+        return a[0]
+    max_c = 0
+    s = a[0]
+    for ep in a:
+        if ep.count(f"{episode:02d}") > max_c:
+            max_c = ep.count(f"{episode:02d}")
+            s = ep
+    return s
+
+
+def find_path(name: str, season: int, episode: int) -> str:
+    directory = find_dir(name)
+    if season:
+        directory = directory + "/" + "season" + f"{season:02d}"
+    return find_ep(directory, episode)
+
+
+def find_sub(name: str, season: int, episode: int) -> list:
+    directory = find_dir(name)
+    return [x for x in glob(directory + "/" + "**", recursive=True) if is_s_match(name, season) and
+            is_sub_file(x) and is_S_and_E_match(x, season, episode)]
+
+    # TODO a better condition like: season01/20.faln.720.srt vs 1.flan.720.str
+    # return [x for x in glob(directory + "/" + "**",recursive=True) if is_sub_file(x) and
+    #       (x.count(f"{season:02d}") + x.count(f"{episode:02d}")) > 1]
+
+
+def is_video(name: str) -> bool:
+    return (name.endswith(".mkv") or name.endswith(".mp4")) and os.path.isfile(name)
+
+
+def is_sub_file(name: str) -> bool:
+    return name.endswith(".srt") and os.path.isfile(name)
+
+
+def is_S_and_E_match(name: str, season: int, episode: int) -> bool:
+    if s_e := re.findall("[sS]\d+[eE]\d+", name):
+        s_e = re.findall("\d+", s_e[0])
+        if int(s_e[0]) != season or int(s_e[1]) != episode:
+            return False
+    return True
+
+
+def is_s_match(name: str, season: int) -> bool:
+    if s := re.findall("[sS]eason\d+", name):
+        s = re.findall("\d+", s[0])
+        if int(s[0]) != season:
+            return False
+    return True
---
+++ play.py
@@ -0,0 +1,49 @@
+import json
+import os
+import subprocess
+import sys
+from os.path import expanduser
+from find import find_path, find_sub
+
+
+def dmenu_chose(s: list) -> str:
+    subs = ""
+    for i in s:
+        subs += i
+        subs += "\n"
+    a = os.popen("echo " + "\"" + subs[:-1] + "\"" + "|" + "dmenu")
+    return a.read()[:-1]
+
+
+def play(name: str, season: int, episode: int):
+    path = find_path(name, season, episode)
+    # path = find_path(name, 0, int(sys.argv[2]))
+    if is_sub:
+        sub_list = find_sub(name, season, episode)
+        if sub_list:
+            if len(sub_list) > 1:
+                sub_list[0] = dmenu_chose(sub_list)
+            subprocess.run([player, path,options, "--sub-file=" + sub_list[0]])
+            return
+    subprocess.run([player,path,options])
+
+
+def main():
+    print(sys.argv[0][:-7])
+    play(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+
+if __name__ == "__main__":
+    with open(sys.argv[0][:-7] + ".data.json") as f:
+        data = json.load(f)
+    conf = data["conf"]
+    print(conf)
+    video_dir = expanduser(conf["video_dir"])
+    os.chdir(video_dir)
+    player = conf["player"]
+    options = conf["options"]
+    if conf["is_full_screen"]:
+       options += "--fs"
+    print(options)
+    is_sub = conf["open_sub"]
+    main()
